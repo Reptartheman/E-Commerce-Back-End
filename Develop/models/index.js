e004296f02baa8b08fc244c4006b8bfb6b604ad4
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-26 15:33:12 -0400

    Update index.js
    Relationships between tables were set in this push.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+// One-to-One relationship
+// Foreign key will belong to Product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// One-to-Many relationship
+// Foreign key will belong to Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Many-to-Many relationship
+// There is a many-to-many relationship between Product and Tag, with ProductTag being the junction table. 
+// Junction Table: This is a table where two tables will be connected because of a many-to-many relationship.
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// Same as above.
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
@@ -18,3 +36,12 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+
+/* NOTES
+
+    -- One to One: It's when one record of one table is linked to another record in another table. 
+    -- One to Many: This is when one record of one table can be linked to zero or more records in another table.
+    -- Many to Many: When many rows in one table are linked to many rows in another table.
+*/
